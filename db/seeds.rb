46cd917c0dc82eb467afd04f5e3d443f6e82f054
Emily Port <emilylport@gmail.com>
2023-02-21 14:33:09 -0700

    add dummy data to seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do 
+  customer = Customer.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, address: Faker::Address.full_address)
+  tea = Tea.create(title: Faker::Beer.brand , temperature: Faker::Number.number(digits: 3), brew_time: Faker::Number.number(digits: 2))
+  5.times do 
+    subscription = Subscription.create(customer_id: customer.id, tea_id: tea.id, title: Faker::Beer.brand , price: Faker::Number.decimal(l_digits: 2, r_digits: 2), status: "Active" , frequency: Faker::Number.number(digits: 2))
+  end
+end
