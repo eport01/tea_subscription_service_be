1e68c5394306a54018d0bec58c8ed8f1f666a796
Emily Port <emilylport@gmail.com>
2023-02-22 15:23:07 -0700

    endpoint to cancel a subscription for a customer

--- spec/requests/subscriptions_requests_spec.rb
+++ spec/requests/subscriptions_requests_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe 'subscription endpoints' do 
 
   describe 'create customer subscriptions' do 
-    it 'creates a new tea subscription for a customer' do 
+    it 'creates a new tea subscription for a customer, default status to active' do 
       customer = Customer.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, address: Faker::Address.full_address)
       subscription = {
         title: "Mint",
@@ -38,7 +38,7 @@ describe 'subscription endpoints' do
       subscription_1 = customer.subscriptions.create(title: Faker::Beer.brand, price: Faker::Number.decimal(l_digits: 2, r_digits: 2), status: "Active" , frequency: Faker::Number.number(digits: 2))
       subscription_2 = customer.subscriptions.create(title: Faker::Beer.brand, price: Faker::Number.decimal(l_digits: 2, r_digits: 2), status: "Active" , frequency: Faker::Number.number(digits: 2))
 
-      get "http://localhost:3000/api/v1/customers/#{customer.id}/subscriptions"
+      get "/api/v1/customers/#{customer.id}/subscriptions"
 
       expect(response).to be_successful 
       
@@ -63,6 +63,32 @@ describe 'subscription endpoints' do
       expect(parsed_subscriptions[0][:attributes]).to have_key(:frequency)
       expect(parsed_subscriptions[0][:attributes][:frequency]).to eq(subscription_1.frequency)
 
+
+    end
+  end
+
+  describe 'cancel subscription endpoint' do 
+    it 'changes a customers subscription status to cancelelled' do 
+      customer = Customer.create(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name, email: Faker::Internet.email, address: Faker::Address.full_address)
+      subscription_1 = customer.subscriptions.create(title: Faker::Beer.brand, price: Faker::Number.decimal(l_digits: 2, r_digits: 2), status: "Active" , frequency: Faker::Number.number(digits: 2))
+      subscription_2 = customer.subscriptions.create(title: Faker::Beer.brand, price: Faker::Number.decimal(l_digits: 2, r_digits: 2), status: "Active" , frequency: Faker::Number.number(digits: 2))
+    
+      expect(subscription_1.status).to eq("Active")
+      expect(subscription_2.status).to eq("Active")
+
+      cancel_subscription = {
+        "status": "Cancelled"
+      }
+
+      patch "/api/v1/customers/#{customer.id}/subscriptions/#{subscription_1.id}", headers: {'CONTENT_TYPE' => 'application/json' }, params: JSON.generate(cancel_subscription)
+      
+      parsed_subscription = JSON.parse(response.body, symbolize_names: true)[:data][:attributes]
+
+      expect(response). to be_successful
+      expect(response.status).to eq(200)
+
+      expect(parsed_subscription[:status]).to eq("Cancelled")
+      expect(subscription_2.status).to eq("Active")
 
     end
   end
